6115f9789ca071d9ba2bfbfd4cf3c0b40feb9677
Norberto <norgit@protonmail.com>
2021-11-11 14:13:19 -0300

    [create: dirs|files] make exercise js07

---
+++ README.md
@@ -0,0 +1,40 @@
+# Ejercicios S-03 JS07
+
+### Ejercicio N°1:
+`str = "origin"`
+cambiar las vocales por el numero 1
+
+```
+output => 1r1g1n
+```
+
+### Ejercicio N°2:
+Crear una función que acepte un parámetro con los valores desde 1 hasta 5.
+si el valor numérico ingresado es 1 mostrar una lista del 1 al 10, si es 2 mostrar del 2 al 10, teniendo en cuenta el limite es 10.
+
+Posibles salidas esperadas:
+
+```
+output => 1...10
+output => 2...10
+output => 3...10
+output => 4...10
+output => 5...10
+```
+
+### Ejercicio N°3:
+`arr = [10, "20", 30, "40", 50]`
+verificar cada elemento sea de  tipo number y sino lo es cambiar su  tipo a number.
+
+```
+output => [10, 20, 30, 40, 50]
+```
+
+### Ejercicio N°4
+`arr = [2, 4, 7, 1, 2, "foo", 7, "bar", "qux", 3, "foo"]`
+eliminar los elementos repetidos.
+nota: (el segundo elemento repetido)
+
+```
+output => [2, 4, 7, 1, "foo", "bar", "qux", 3]
+```
---
+++ js/main.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const toTop = document.querySelector('#toTop')
+
+	window.addEventListener('scroll', controlScroll)
+
+	function controlScroll() {
+		const st = document.body.scrollTop || document.documentElement.scrollTop
+		const display = toTop.style.display
+		const cl = toTop.classList
+		if (st > 300) {
+			toTop.style.display = 'block'
+			cl.remove('hide')
+			cl.add('show')
+			setTimeout(() => {cl.add('rotate')}, 2100)
+	  } else {
+			if (display === 'block') {
+				cl.remove('show')
+				cl.add('hide')
+				setTimeout(() => {
+					cl.remove('rotate')
+					toTop.style.display = 'none'
+				}, 2000)
+			}
+	  }
+	}
+
+	toTop.addEventListener('click', () => {
+		document.body.scrollTop = 0
+		document.documentElement.scrollTop = 0
+	})
+
+	const ej01 = (str = 'origin') => str.split('').map(char => (['a', 'e', 'i', 'o', 'u'].includes(char) ? '1' : char)).join('')
+	const ej02 = num => {
+		const nums = []
+		for (let i = num; i <= 10; i++) {
+			nums.push(i)
+		}
+		return nums
+	}
+	const ej03 = (arr = [10, '20', 30, '40', 50]) => arr.map(el => typeof el === 'string' ? Number(el) : el)
+	const ej04 = (arr = [2, 4, 7, 1, 2, 'foo', 7, 'bar', 'qux', 3, 'foo']) => [...new Set(arr)]
+
+	document.addEventListener('click', e => {
+		const { target: { tagName, id } } = e
+		if (tagName === 'BUTTON') {
+			switch (id) {
+				case 'ej01':
+					return alert(ej01())
+				case 'ej02':
+					const p2 = parseInt(prompt('Ingresá un dígito del 1 al 10: '))
+					return alert(ej02(p2))
+				case 'ej03':
+					return alert(ej03())
+				case 'ej04':
+					return alert(ej04())
+				default:
+					console.log('ELEGIR EJERCICIO')
+			}
+		}
+	})
+
+})
